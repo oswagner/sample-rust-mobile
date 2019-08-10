d889758e55bf09da74c7b11a089768076b9bad37
naomijub <jnaomi@thoughtworks.com>
2019-08-10 16:40:51 -0300

    Insert new struct

--- todo-server/src/main.rs
+++ todo-server/src/main.rs
@@ -28,6 +28,7 @@ fn main() {
   let no_compression: CurrentSession =
     new_session(&cluster_config, RoundRobin::new()).expect("session should be created");
 
+  insert_struct(&no_compression);
   select_struct(&no_compression);
 }
 
@@ -35,6 +36,12 @@ fn main() {
 struct Person {
   id: Uuid,
   person: String,
+}
+
+impl Person {
+  fn into_query_values(self) -> QueryValues {
+    query_values!("id" => self.id, "person" => self.person)
+  }
 }
 
 fn select_struct(session: &CurrentSession) {
@@ -51,4 +58,17 @@ fn select_struct(session: &CurrentSession) {
     let my_row: Person = Person::try_from_row(row).expect("into RowStruct");
     println!("struct got: {:?}", my_row);
   }
+}
+
+fn insert_struct(session: &CurrentSession) {
+  let row = Person {
+    person: String::from("Julia"),
+    id: Uuid::new_v4()
+  };
+
+  let insert_struct_cql = "INSERT INTO todo.person \
+                           (id, person) VALUES (?, ?)";
+  session
+    .query_with_values(insert_struct_cql, row.into_query_values())
+    .expect("insert");
 }

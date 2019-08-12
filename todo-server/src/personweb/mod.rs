17bd72cf84ea5b2897f97e3746961261dde4e375
naomijub <jnaomi@thoughtworks.com>
2019-08-11 23:54:28 -0300

    Makes select id safe for failing ids

--- todo-server/src/personweb/mod.rs
+++ todo-server/src/personweb/mod.rs
@@ -17,9 +17,15 @@ pub fn select_all(data: web::Data<AppState>) -> Box<dyn Future<Item = HttpRespon
 }
 
 pub fn select_id(info: web::Path<(String)>, data: web::Data<AppState>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
-    Box::new(ok::<_, Error>(
-        HttpResponse::Ok().content_type("application/json").body(select_person_id(&data.cql_session, info.to_string()))
-    ))
+    let re = Regex::new(r"^\w{8}\-(\w{4}\-){3}\w{12}$").unwrap();
+    match re.is_match(&info.to_string()) {
+      true => Box::new(ok::<_, Error>(
+          HttpResponse::Ok().content_type("application/json").body(select_person_id(&data.cql_session, info.to_string()))
+        )),
+      false => Box::new(ok::<_, Error>(
+          HttpResponse::BadRequest().body("Id must be a UUID V4 format.")
+        ))
+    }
 }
 
 pub fn add_person(info: web::Json<Info>, data: web::Data<AppState>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
@@ -28,12 +34,11 @@ pub fn add_person(info: web::Json<Info>, data: web::Data<AppState>) -> Box<dyn F
       true => {
         let id = insert_person(&data.cql_session, format!("{}",info.name));
         Box::new(ok::<_, Error>(
-          HttpResponse::Ok().content_type("application/json").body(id)
+          HttpResponse::Created().content_type("application/json").body(id)
         ))},
-      false => {
+      false => 
         Box::new(ok::<_, Error>(
           HttpResponse::BadRequest().body("Name must contain only letter, numbers and underscore.")
-        ))
-      },
+        )),
     }
 }
